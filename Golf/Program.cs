cef164ae3034c5c4bf790dd8becdba574b611e67
Mikael Lindström <mikael.lindstrom@lindstromsmanagement.se>
2024-02-13 16:41:00 +0100

    Welcome the user to this program

--- Golf/Program.cs
+++ Golf/Program.cs
@@ -2,11 +2,27 @@
 
 namespace Golf
 {
-    internal class Program
+    /// <summary>
+    /// The class for this program
+    /// </summary>
+    public class Program
     {
+        /// <summary>
+        /// The name of this program
+        /// </summary>
+        public const string ProgramName = "The Golfswing 2000";
+        
+        /// <summary>
+        /// The start point of this program
+        /// </summary>
+        /// <param name="args">Anything</param>
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Welcome the user to this program
+            Console.WriteLine($"Welcome to {ProgramName}!");
+
+            // Make a pause in the program
+            Console.ReadKey();
         }
     }
 }
